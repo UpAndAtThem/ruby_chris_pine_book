e2f64e1812245e03ebe48b0795270aca7d47bb90
Jason Nelson <Jasonn318@gmail.com>
2018-04-17 14:08:33 -0500

    add assoc.rb

---
+++ assoc.rb
@@ -0,0 +1,7 @@
+#Turn this into an array containing only two elements: Barney's name and Barney's number
+
+h = {"colors"  => ["red", "blue", "green"],
+     "letters" => ["a", "b", "c" ],
+     "numbers" => [1, 2, 3]}
+
+puts h.assoc('numbers')
