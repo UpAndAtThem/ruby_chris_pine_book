8c2a11a077f0f36f29e9a7f46710c0b22eb2287c
Jason Nelson <Jasonn318@gmail.com>
2018-04-17 14:29:58 -0500

    add shorthand_arr_initialization.rb

---
+++ shorthand_arr_initialization.rb
@@ -0,0 +1,7 @@
+#Show an easier way to write this array:
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones_shorthand = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+puts flintstones == flintstones_shorthand
